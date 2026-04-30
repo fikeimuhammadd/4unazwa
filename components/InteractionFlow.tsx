6fa72f882ee4529e3c3d88ff62fee1bc15d4a668
fikeimuhammadd <74227981+fikeimuhammadd@users.noreply.github.com>
2026-05-01 02:09:11 +0700

    Update InteractionFlow.tsx

--- components/InteractionFlow.tsx
+++ components/InteractionFlow.tsx
@@ -340,7 +340,7 @@ const LoveMeterStep = ({ onComplete }: { onComplete: () => void }) => {
 
 // --- Step 4: Typewriter ---
 const TypewriterStep = ({ onComplete }: { onComplete: () => void }) => {
-    const text = "Happy Valentine!!!!";
+    const text = "Haii nazwa acuu hihi, love you!";
     const [displayedText, setDisplayedText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
 
